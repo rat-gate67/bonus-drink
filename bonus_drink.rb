30d8671074e77c7943edfa79201bd95dd72dc39c
rat <rat.gate64@gmail.com>
2024-05-21 23:06:55 +0900

    Complete the answer

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,6 +1,10 @@
 class BonusDrink
   def self.total_count_for(amount)
-    
-    0
+    sum = 0
+    while amount >= 3
+      sum += amount - amount % 3
+      amount = amount / 3 + amount % 3
+    end
+    return sum + amount
   end
 end
